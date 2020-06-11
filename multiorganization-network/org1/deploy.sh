9f9040b786551cbb8887220cf978ff91aeac2826
Jorge Rodriguez Flores <jorgeedison1@gmail.com>
2020-06-11 08:09:39 +0000

    Node organización 1

--- multiorganization-network/org1/deploy.sh
+++ multiorganization-network/org1/deploy.sh
@@ -1,3 +1,5 @@
 docker stack deploy -c ca.yml ca_org1
 
 docker stack deploy -c peer0.yml peer0_org1
+
+docker stack deploy -c peer1.yml peer1_org1
--- multiorganization-network/org1/stop.sh
+++ multiorganization-network/org1/stop.sh
@@ -1,3 +1,5 @@
 docker stack rm ca_org1
 
 docker stack rm peer0_org1
+
+docker stack rm peer1_org1
